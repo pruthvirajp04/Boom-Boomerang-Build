586fb7bedb9439797b14a55f25dc8757f518bae8
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-09-13 01:14:44 +0530

    reward ad

--- js/bundle.js
+++ js/bundle.js
@@ -1292,8 +1292,23 @@ var GameMgr = /** @class */ (function (_super) {
             replayInstance.destroyAd();
             replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameMgr.prototype.rewardedCallbacks);
         }
-   
-    
+        if(sessionStorage.getItem("reward-type") == "replay-RP1"){
+            sessionStorage.removeItem("reward-type");
+            sessionStorage.setItem("doneReplay",1);
+            if(replayInstance != undefined)
+            replayInstance.destroyAd();
+            replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameMgr.prototype.rewardedCallbacks);
+        }
+        if(sessionStorage.getItem("reward-type") == "replay-RP2"){
+            sessionStorage.removeItem("reward-type");
+            let level = parseInt(sessionStorage.getItem("SelectedLevel"));
+            sendCustomAnalyticsEvent('game_end', {level: level});
+            sendCustomAnalyticsEvent("game_replay", {level: level});
+            sendCustomAnalyticsEvent("game_level", {level: level});
+            if(replayInstance != undefined)
+            replayInstance.destroyAd();
+            replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameMgr.prototype.rewardedCallbacks);
+        }
         if(sessionStorage.getItem("reward-type") == "replay-BK"){
             sessionStorage.removeItem("reward-type");
             if(replayInstance != undefined)
@@ -1334,7 +1349,7 @@ var GameMgr = /** @class */ (function (_super) {
             this._currentDia = v;
             sessionStorage.setItem("CurrDiamond",v);
             if (!is_rewarded_noFill) {
-                sessionStorage.setItem("reward-type","replay-RP");
+                sessionStorage.setItem("reward-type","reward-SL");
                 Laya.SoundManager.muted = true;
                 window.GlanceGamingAdInterface.showRewarededAd(rewardInstance);
             } 
@@ -7245,141 +7260,17 @@ var GameOver = /** @class */ (function (_super) {
             }
         }, null);
     };
-    GameOver.prototype.rewardedCallbacks = function (obj) {
-        var self = this;
-        obj.adInstance?.registerCallback('onAdLoadSucceed', (data) => {
-            //console.log('onAdLoadSucceeded Rewarded CALLBACK', data);
-            if (obj.adUnitName === rewardObj.adUnitName) {
-                is_rewarded_noFill = false
-            }
-            if (obj.adUnitName === replayObj.adUnitName) {
-                is_replay_noFill = false
-            }
-        });
-        
-        obj.adInstance?.registerCallback('onAdLoadFailed', (data) => {
-            //console.log('onAdLoadFailed Rewarded CALLBACK', data);
-            if (obj.adUnitName === rewardObj.adUnitName) {
-                is_rewarded_noFill = true
-            }
-            if (obj.adUnitName === replayObj.adUnitName) {
-                is_replay_noFill = true
-            }
-    
-    
-        });
-    
-        obj.adInstance?.registerCallback('onAdDisplayed', (data) => {
-            //console.log('onAdDisplayed Rewarded CALLBACK', data);
-    
-    
-        });
-
-        obj.adInstance?.registerCallback('onAdClicked', (data) => {
-            //console.log('onAdClicked Rewarded CALLBACK', data);
-        });
-        
-        obj.adInstance?.registerCallback('onAdClosed', (data) => {
-            if(sessionStorage.getItem("sound_status") == 1)
-            Laya.SoundManager.muted = false;
-            //console.log('onAdClosed Rewarded CALLBACK', data);
-
-        if(sessionStorage.getItem("reward-type") == "reward-SL"){
-            sessionStorage.removeItem("reward-type");
-            if(rewardInstance != undefined)
-            rewardInstance.destroyAd();
-            if (obj.adUnitName == rewardObj.adUnitName) {
-                isRewardedAdClosedByUser = true
-            }
-            rewardInstance=window.GlanceGamingAdInterface.loadRewardedAd(rewardObj,GameOver.prototype.rewardedCallbacks);
-            if(!isRewardGranted && isRewardedAdClosedByUser)
-            {  
-                cancelRewardSL(); 
-            }
-            else{ 
-                giveRewardSL();
-            }
-            isRewardGranted = false
-            isRewardedAdClosedByUser = false
-    
-        }
-        if(sessionStorage.getItem("reward-type") == "reward-SH"){
-            sessionStorage.removeItem("reward-type");
-            if(rewardInstance != undefined)
-            rewardInstance.destroyAd();
-            if (obj.adUnitName == rewardObj.adUnitName) {
-                isRewardedAdClosedByUser = true
-            }
-            rewardInstance=window.GlanceGamingAdInterface.loadRewardedAd(rewardObj,GameOver.prototype.rewardedCallbacks);
-            if(!isRewardGranted && isRewardedAdClosedByUser)
-            {  
-                cancelRewardSH();
-            }
-            else{ 
-                giveRewardSH();
-            }
-            isRewardGranted = false
-            isRewardedAdClosedByUser = false
-        }
-        if(sessionStorage.getItem("reward-type") == "reward-CL"){
-            sessionStorage.removeItem("reward-type");
-            if(rewardInstance != undefined)
-            rewardInstance.destroyAd();
-            if (obj.adUnitName == rewardObj.adUnitName) {
-                isRewardedAdClosedByUser = true
-            }
-            rewardInstance=window.GlanceGamingAdInterface.loadRewardedAd(rewardObj,GameOver.prototype.rewardedCallbacks);
-            if(!isRewardGranted && isRewardedAdClosedByUser)
-            {  
-                cancelRewardCL();
-            }
-            else{ 
-                giveRewardCL();
-            }
-            isRewardGranted = false
-            isRewardedAdClosedByUser = false
-    
-        }
-
-        if(sessionStorage.getItem("reward-type") == "replay-RP3"){
-            sessionStorage.removeItem("reward-type");
-            let level = parseInt(sessionStorage.getItem("SelectedLevel"));
-            sendCustomAnalyticsEvent('game_end', {level: level});
-            sendCustomAnalyticsEvent("game_replay", {level: level});
-            sendCustomAnalyticsEvent("game_level", {level: level});
-            if(replayInstance != undefined)
-            replayInstance.destroyAd();
-            replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameOver.prototype.rewardedCallbacks);
-        }
-        if(sessionStorage.getItem("reward-type") == "replay-BK"){
-            sessionStorage.removeItem("reward-type");
-            if(replayInstance != undefined)
-            replayInstance.destroyAd();
-            replayInstance = window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameOver.prototype.rewardedCallbacks);
-        }
-        });
-
-        obj.adInstance?.registerCallback('onRewardsUnlocked', (data) => {
-            //console.log('onRewardsUnlocked Rewarded CALLBACK', data);
-    
-            if (obj.adUnitName === rewardObj.adUnitName) {
-                isRewardGranted = true
-            }
-    
-        });
-
-}
     GameOver.prototype.ReturnMain = function () {
         // alert("df")
         if (!is_replay_noFill) {
-            sessionStorage.setItem("reward-type","replay-RP3");
+            sessionStorage.setItem("reward-type","replay-RP");
             Laya.SoundManager.muted = true;
             window.GlanceGamingAdInterface.showRewarededAd(replayInstance);
         }else{
 
             if(replayInstance != undefined)
             replayInstance.destroyAd();
-            replayInstance=window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameOver.prototype.rewardedCallbacks);
+            replayInstance=window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameMgr.prototype.rewardedCallbacks);
         } 
         var data = {};
         data.onCloseEvent = function () {
@@ -7602,7 +7493,7 @@ var GamePlaying = /** @class */ (function (_super) {
             }else{
                 if(replayInstance != undefined)
                 replayInstance.destroyAd();
-                replayInstance=window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GamePlaying.prototype.rewardedCallbacks);
+                replayInstance=window.GlanceGamingAdInterface.loadRewardedAd(replayObj, GameMgr.prototype.rewardedCallbacks);
             } 
         }
         Laya.timer.frameLoop(1, this, function () {
