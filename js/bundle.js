00bea3d5b1625ab29a8aed18892bfcbb8c3966fd
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-09-13 01:27:28 +0530

    level ad fix

--- js/bundle.js
+++ js/bundle.js
@@ -7212,15 +7212,7 @@ var GameOver = /** @class */ (function (_super) {
     };
     GameOver.prototype.ReturnMain = function () {
         // alert("df")
-        if (!is_replay_noFill) {
-            sessionStorage.setItem("reward-type","replay-RP");
-            Laya.SoundManager.muted = true;
-            window.GlanceGamingAdInterface.showRewarededAd(replayInstance);
-        }else{
-
-            if(replayInstance != undefined)
-            replayInstance.destroyAd();
-        } 
+    
         var data = {};
         data.onCloseEvent = function () {
             ViewMgr_1.default.instance.openView(ViewMgr_1.ViewDef.GameMain);
